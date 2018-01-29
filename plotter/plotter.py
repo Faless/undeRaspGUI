c17af7a61ad7e5081486a6b40063fe77939590a3
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-01-29 16:47:44 +0100

    Make plotter a module. Refactor it to functions

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -2,52 +2,48 @@ from mpl_toolkits.axes_grid1 import host_subplot
 import mpl_toolkits.axisartist as AA
 import matplotlib.pyplot as plt
 import sys
+from os import path
 
-file=sys.argv[1]
-print file
+def get_dict_from_file(filename):
+    f = open(filename, 'rt')
 
+    ctd={
+      "Depth":[],
+      "Temp":[],
+      "Cond":[],
+      "Sal.":[],
+      "O2Sat%":[],
+      "O2ppm":[],
+      "pH":[],
+      "Eh":[],
+      "Date":[]}
 
-f = open(file, 'rt')
-ctd={
-  "Depth":[],
-  "Temp":[],
-  "Cond":[],
-  "Sal.":[],
-  "O2Sat%":[],
-  "O2ppm":[],
-  "pH":[],
-  "Eh":[],
-  "Date":[]}
-i=0
-try:
-   for line in f.readlines():
-      if i>0 and len(line)>1:
-         ctd["Depth"].append(float(line[:7]))        
-         ctd["Temp"].append(float(line[8:15]))
-         ctd["Cond"].append(float(line[16:24]))        
-         ctd["Sal."].append(float(line[25:33]))        
-         ctd["O2Sat%"].append(float(line[34:40]))        
-         ctd["O2ppm"].append(float(line[41:46]))        
-         ctd["pH"].append(float(line[47:53]))        
-         ctd["Eh"].append(float(line[54:59]))          
-         ctd["Date"].append(line[60:])        
-      i=i+1
-finally:
-    f.close()
-    ctd["Depth"]=[x*-1 for x in ctd["Depth"]]
+    i=0
+    try:
+       for line in f.readlines():
+          if i>0 and len(line)>1:
+             ctd["Depth"].append(float(line[:7]))
+             ctd["Temp"].append(float(line[8:15]))
+             ctd["Cond"].append(float(line[16:24]))
+             ctd["Sal."].append(float(line[25:33]))
+             ctd["O2Sat%"].append(float(line[34:40]))
+             ctd["O2ppm"].append(float(line[41:46]))
+             ctd["pH"].append(float(line[47:53]))
+             ctd["Eh"].append(float(line[54:59]))
+             ctd["Date"].append(line[60:])
+          i=i+1
+    finally:
+        f.close()
+        ctd["Depth"]=[x*-1 for x in ctd["Depth"]]
 
-    
+    return ctd
 
-
-if 1:
-
-
+def get_plot_from_dict(ctd):
     host = host_subplot(111, axes_class=AA.Axes)
     plt.subplots_adjust(bottom=0.4)
     plt.subplots_adjust(left=0.1)
     plt.subplots_adjust(right=0.9)
     plt.subplots_adjust(top=0.8)
-
 
     sal = host.twiny()
     ph = host.twiny()
@@ -59,7 +55,6 @@ if 1:
     p3, = ph.plot(ctd["pH"],ctd["Depth"],label="pH")
     p4, = o2.plot(ctd["O2Sat%"],ctd["Depth"],label="O2")
     p5, = eh.plot(ctd["Eh"],ctd["Depth"],label="Eh")
-
 
     offset = -40
     new_fixed_axis = ph.get_grid_helper().new_fixed_axis
@@ -74,10 +69,8 @@ if 1:
     eh.axis["top"] = new_fixed_axis(loc="top", axes=eh, offset=(0, -1*offset))
     eh.axis["bottom"].toggle(all=False)
 
-
     host.set_xlim(0, max(ctd["Temp"])+1)
     host.set_ylim(min(ctd["Depth"])-1,0)
-
 
     host.set_ylabel("Depth")
     host.set_xlabel("Temperature")
@@ -86,15 +79,10 @@ if 1:
     o2.set_xlabel("O2 perc")
     eh.set_xlabel("Eh")
 
-
-
-
     sal.set_xlim(min(ctd["Sal."])-1,max(ctd["Sal."])+1)
     eh.set_xlim(min(ctd["Eh"])-20,max(ctd["Eh"])+20)
     ph.set_xlim(0, 14)
     o2.set_xlim(0, 100)
-
-#    host.legend()
 
     host.axis["bottom"].label.set_color(p1.get_color())
     sal.axis["top"].label.set_color(p2.get_color())
@@ -102,10 +90,14 @@ if 1:
     o2.axis["bottom"].label.set_color(p4.get_color())
     eh.axis["top"].label.set_color(p5.get_color())
 
+    return plt
 
+if __name__ == "__main__":
+    txt = sys.argv[1]
+    print("Reading: %s" % txt)
+    ctd = get_dict_from_file(txt)
+    plt = get_plot_from_dict(ctd)
     plt.draw()
-#    fig=plt.draw() #.figure()
-    filename=file[:-3]+"jpg"
-    plt.savefig(filename)
-#    plt.draw()
-#    plt.show()
+    image=path.splitext(txt)[0] + ".jpg"
+    print("Saving: %s" % image)
+    plt.savefig(image)
